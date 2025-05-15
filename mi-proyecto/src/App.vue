cdd84dbe0fd988ca737686d8219477edc88a6e8a
Franpall <skfranpal@gmail.com>
2025-05-14 20:42:23 -0400

    Creación de proyecto VITE

---
+++ mi-proyecto/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from './components/AppHeader.vue'
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+</style>
---
+++ mi-proyecto/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <header>
+    <h1>Aware Digital</h1>
+  </header>
+</template>
+
+<style scoped>
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+  background-color: #0e4da0;
+}
+</style>
